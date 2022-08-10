1c9a0e61dc50f5a89f617f825b0f5c388f7e98a3
Vitor <sblvitor@gmail.com>
2022-08-10 17:26:54 -0400

    estruturacao do app

--- app/src/main/java/com/lira/repositoriesapp/ui/MainActivity.kt
+++ app/src/main/java/com/lira/repositoriesapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lira.repositoriesapp
+package com.lira.repositoriesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lira.repositoriesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
